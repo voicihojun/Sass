7f5664a8b9e7829cd853df285cfbf52f5186b4fa
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-03-22 17:03:46 -0400

    04. fullWidth, outline

--- src/App.js
+++ src/App.js
@@ -27,6 +27,28 @@ function App() {
           BUTTON
         </Button>
       </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" color="pink" fullWidth>
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
